4527c8c3915ea5b0ee6a9fc08a0c78892ad00466
WalterHeitor <waterheitorfo@gmail.com>
2024-03-31 18:16:44 -0300

    feat: implementacao de local stack

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -7,3 +7,7 @@ V
 &app/src/main/java/org/example/App.java,f/0/f0a978cc1cbbf6e07638768c31447a4bd667c80f
 `
 0app/src/main/java/org/example/entity/Person.java,4/1/41660e729b29c3d4ac4cd06f84d2927d38b53458
+M
+localstack/docker-compose.yml,4/3/43e214733f81bfe7deff13772d0bda1461b11aa0
+C
+localstack/start.sh,e/3/e375730ca642c0d0642aa83646d674ee721930c9
---
+++ localstack/start.sh
@@ -0,0 +1,59 @@
+
+
+aws --endpoint-url=http://localhost:4566 s3 mb s3://my-test-bucket
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name my-test-queue
+
+# O Lambda precisa de um IAM Role para a sua execução
+aws --endpoint http://localhost:4566 --profile localstack \
+  iam create-role \
+  --role-name lambda-execution \
+  --assume-role-policy-document "{\"Version\": \"2012-10-17\",\"Statement\": [{ \"Effect\": \"Allow\", \"Principal\": {\"Service\": \"lambda.amazonaws.com\"}, \"Action\": \"sts:AssumeRole\"}]}"
+
+  # Com a IAM Role criada, vamos incluir a policy de execução:
+aws --endpoint http://localhost:4566 --profile localstack \
+  iam attach-role-policy \
+  --role-name lambda-execution \
+  --policy-arn arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole
+
+# Da acesso para lambda ler o arquivo do S3
+aws --endpoint http://localhost:4566 --profile localstack \
+  iam attach-role-policy \
+  --role-name lambda-execution \
+  --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
+
+#  implantação do Lambda no LocalStack com o seguinte comando:
+aws --endpoint http://localhost:4566 --profile localstack \
+  --region us-east-1 lambda create-function \
+  --function-name validacao \
+  --runtime java17 \
+  --handler br.com.softwalter.App \
+  --memory-size 128 \
+  --zip-file fileb://validate_file/app/build/libs/app.jar \
+  --role arn:aws:iam::000000000000:role/lambda-execution --timeout 30000
+
+# Configuração do gatilho S3 para acionar a função Lambda
+aws --endpoint-url=http://localhost:4566 lambda add-permission \
+  --function-name validacao \
+  --statement-id s3-event \
+  --action "lambda:InvokeFunction" \
+  --principal s3.amazonaws.com \
+  --source-arn "arn:aws:s3:::my-test-bucket" \
+  --source-account "000000000000" \
+  --region us-east-1
+
+aws --endpoint-url=http://localhost:4566 s3api put-bucket-notification-configuration \
+  --bucket my-test-bucket \
+  --notification-configuration '{"LambdaFunctionConfigurations":[{"LambdaFunctionArn":"arn:aws:lambda:us-east-1:000000000000:function:validacao","Events":["s3:ObjectCreated:*"]}]}'
+
+# subindo arquivo para execucao da lambda
+aws --endpoint-url=http://localhost:4566 s3  cp ./localstack/mock_data.csv s3://my-test-bucket
+
+# Executando o Lambda e verificando se foi instalado corretamente:
+aws --endpoint http://localhost:4566 --profile localstack \
+  lambda invoke \
+  --function-name validacao out.txt \
+  --log-type Tail
+
+
+DATABASE_USERNAME=postgres;DATABASE_PASSWORD=postgres;DATABASE_NAME=postgres;DATABASE_SCHEMA=public;DATABASE_PORT=5432;DATABASE_HOST=localhost;
+
